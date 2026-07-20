de43276ed83485a5f6f4dcff0cd5632d16399278
annapng137 <annaliviagomes13passos@gmail.com>
2026-07-20 17:43:46 -0300

    Add files via upload

---
+++ CHATBOT COM I A/principal.py
@@ -0,0 +1,40 @@
+import os
+import streamlit as st
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv ()
+
+modelo_ia = OpenAI(api_key=os.getenv ("OPENAI_API_KEY") )
+
+st.write ("# Chatbot com IA")
+
+if  not "lista_mensagens" in st.session_state:
+    st.session_state ["lista_mensagens"] = [] 
+
+
+
+for mensagem in st.session_state["lista_mensagens"]: 
+    role = mensagem["role"]
+    content = mensagem["content"] 
+    st.chat_message (role).write(content)
+
+texto_usuario = st.chat_input("Digite sua mensagem aqui...")
+
+if texto_usuario: 
+   
+    st.chat_message ("user").write( texto_usuario )
+    mensagem= {"role" : "user" , "content" : texto_usuario}
+    st.session_state ["lista_mensagens"].append(mensagem)
+    
+
+    resposta_ia = modelo_ia.chat.completions.create(
+        messages=st.session_state["lista_mensagens"],
+        model="gpt-4o"
+    )
+    texto_resposta_ia =  resposta_ia.choices[0].message.content
+   
+    st.chat_message ("assistant").write(texto_resposta_ia)
+    mensagem_ia = {"role" : "assistant" , "content" : texto_resposta_ia}
+    st.session_state["lista_mensagens"].append(mensagem_ia)
+    
